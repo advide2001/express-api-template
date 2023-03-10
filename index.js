1ef3309af9b5f128a1ed047ba29e5ad85173674d
Adarsh Desai <desaiadarsh22@gmail.com>
2023-03-10 18:18:15 +0530

    Implemented mobile navigation for smaller screens

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const navElement = document.querySelector(".primary-navigation");
+const navToggleButton = document.querySelector(".mobile-nav-toggle");
 
+navToggleButton.addEventListener("click", () => {
+  const visible = navElement.getAttribute("data-visible");
+  if (visible === "true") {
+    navElement.setAttribute("data-visible", "false");
+    navToggleButton.style.backgroundImage =
+      "url(../../images/shared/icon-hamburger.svg)";
+    navToggleButton.setAttribute("aria-expanded", false);
+  } else {
+    navElement.setAttribute("data-visible", "true");
+    navToggleButton.style.backgroundImage =
+      "url(../../images/shared/icon-close.svg)";
+    navToggleButton.setAttribute("aria-expanded", true);
+  }
+});
