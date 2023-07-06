2c2bdb5a4a188ee2fc9b365e8f3674d64f64f13f
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-06 08:00:28 +0530

    refactor: Simplify response handling in GET route

--- src/api/todos/todos.route.ts
+++ src/api/todos/todos.route.ts
@@ -1,11 +1,12 @@
 import { Router, Request, Response } from 'express';
-import { Todo, Todos } from './todos.model';
+import { TodoWithId, Todos } from './todos.model';
 
 const router =  Router();
 
-router.get('/', async (req:Request, res:Response<Todo[]>) => {
+router.get('/', async (req:Request, res:Response<TodoWithId[]>) => {
   const result = await Todos.find(); 
-  res.json(result.toArray());
+  const todos  = await result.toArray();
+  res.json(todos);
 });
 
 export default router; 
