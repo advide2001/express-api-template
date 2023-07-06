edbf0e802d0db462a9f84425d39055674eca8305
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-06 07:49:47 +0530

    fix: Add "mongodb://" to the connection string

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const { MONGO_URI = 'localhost/todo-api' } = process.env; 
+const { MONGO_URI = 'mongodb://localhost/todo-api' } = process.env; 
 
 export const client = new MongoClient(MONGO_URI); 
 export const db = client.db();
