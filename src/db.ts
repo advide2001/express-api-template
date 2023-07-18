fdc4d7beed90990866ade5a3c0be1b80db1a446e
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-18 18:55:18 +0530

    feat: Build a new express API template

--- src/db.ts
+++
@@ -1,6 +0,0 @@
-import { MongoClient } from 'mongodb';
-
-const { MONGO_URI = 'mongodb://localhost/todo-api' } = process.env; 
-
-export const client = new MongoClient(MONGO_URI); 
-export const db = client.db();
